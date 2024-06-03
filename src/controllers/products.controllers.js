3d686ec526212ecf41dd4028d5fb5736a5740abb
CARLOSARMANDOLARIOSROJAS <20460440@colima.tecnm.mx>
2024-06-03 14:42:07 -0600

    Config de cloudinary

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -5,6 +5,12 @@ const prisma = new PrismaClient();
 const cloudinary = require('../../cloudinary.js');
 
 
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const getAllProducts = async (req, res) => {
   try {
     const products = await prisma.product.findMany({
